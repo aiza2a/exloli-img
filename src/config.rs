998e9ea4745203aea5c2db9303fd70b4a257ad97
Aizawa <109516527+aiza2a@users.noreply.github.com>
2026-03-02 23:58:52 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -10,61 +10,44 @@ pub static CHANNEL_ID: OnceCell<String> = OnceCell::new();
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Config {
-    /// 日志等级
     pub log_level: String,
-    /// 同时下载线程数量
     pub threads_num: usize,
-    /// 定时爬取间隔
     #[serde(deserialize_with = "deserialize_duration")]
     pub interval: Duration,
-    /// Sqlite 数据库位置
     pub database_url: String,
     pub exhentai: ExHentai,
     pub telegraph: Telegraph,
     pub telegram: Telegram,
-    pub catbox: Catbox,
+    pub freeimage: FreeimageConfig, 
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct FreeimageConfig {
+    pub api_key: String,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct ExHentai {
-    /// 登陆 cookie
     pub cookie: String,
-    /// 搜索参数
     pub search_params: Vec<(String, String)>,
-    /// 最大遍历画廊数量
     pub search_count: usize,
-    /// 翻译文件的位置
     pub trans_file: String,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Telegraph {
-    /// Telegraph token
     pub access_token: String,
-    /// 文章作者名称
     pub author_name: String,
-    /// 文章作者连接
     pub author_url: String,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Telegram {
-    /// 频道 id
     pub channel_id: Recipient,
-    /// bot 名称
     pub bot_id: String,
-    /// bot token
     pub token: String,
-    /// 讨论组 ID
     pub group_id: ChatId,
-    /// 入口讨论组 ID
     pub auth_group_id: ChatId,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-pub struct Catbox {
-    pub userhash: String,
-    pub api_url: String,
 }
 
 impl Config {
